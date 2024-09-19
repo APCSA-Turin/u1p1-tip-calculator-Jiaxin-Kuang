6bfc4e50ada10b0a896053d005504f4504a223b4
Jiaxin-Kuang <jiaxink2@nycstudents.net>
2024-09-19 16:24:27 +0000

    first commit

--- src/main/java/com/example/project/TipCalculator.java
+++ src/main/java/com/example/project/TipCalculator.java
@@ -6,15 +6,20 @@ public class TipCalculator {
         //DO NOT DELETE ANY OF THE CODE BELOW      
         StringBuilder result = new StringBuilder();
         //your code here
-                       
+        //I learned how to round from https://coreui.io/blog/how-to-round-a-number-to-two-decimal-places-in-javascript/#:~:text=Another%20method%20to%20round%20to,then%20dividing%20back%20by%20100.&text=This%20method%20ensures%20the%20result,the%20essence%20of%20mathematical%20operations.
+        double tip = (Math.round(cost*(percent/100.0)*100.0))/100.0;
+        double billWithTip = cost + tip;
+        double costPerPerson = (Math.round(cost/people * 100.0))/100.0;
+        double tipPerPerson =  (Math.round(tip/people * 100.0))/100.0;
+        double totalPerPerson = (Math.round((billWithTip/people) * 100.0))/100.0;
         result.append("-------------------------------\n");
-        result.append("Total bill before tip: "); //concatenate to this string. DO NOT ERASE AND REWRITE
-        result.append("Total percentage: ");
-        result.append("Total tip: ");
-        result.append("Total Bill with tip: ");
-        result.append("Per person cost before tip: ");
-        result.append("Tip per person: ");
-        result.append("Total cost per person: ");
+        result.append("Total bill before tip: " + "$" + cost + "\n"); //concatenate to this string. DO NOT ERASE AND REWRITE
+        result.append("Total percentage: " + percent + "%\n");
+        result.append("Total tip: " + "$" + tip + "\n");
+        result.append("Total Bill with tip: " + "$" + billWithTip + "\n");
+        result.append("Per person cost before tip: " + "$" + costPerPerson +"\n");
+        result.append("Tip per person: " + "$" + tipPerPerson + "\n");
+        result.append("Total cost per person: " + "$" + totalPerPerson + "\n");
         result.append("-------------------------------\n");
 
 
@@ -23,9 +28,9 @@ public class TipCalculator {
      //TEST YOUR PROGRAM IN main
      public static void main(String[] args) {
         //try different values for people, percent, and cost to test your program before running test cases
-        int people; 
-        int percent;
-        double cost;              
+        int people = 2;
+        int percent = 5;
+        double cost = 29.56;            
         System.out.println(calculateTip(people,percent,cost));
     }
 }
